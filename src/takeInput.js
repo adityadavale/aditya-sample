a2d7dd8cce35c926d3e81c7d1f6d5cb747b8550f
Aditya Davale <aditya.davale@2020spaces.com>
2022-02-22 10:48:09 +0530

    taking input

--- src/takeInput.js
+++ src/takeInput.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 class Takeinput extends React.Component {
+
+  onSubmithandler = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  }
+
   render() {
-    return (<div> Hello Class Component</div >
+    return (
+      <>
+        <form onSubmit={this.onSubmithandler}>
+          <div>Welcome to Form</div>
+          <input type="text" placeholder="Enter your Name"/>
+          <button type="text">Add Name </button>
+        </form>
+      </>
     )
   }
 }
